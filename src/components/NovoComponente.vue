b3b31948d430da65ebff18b4899187acafd2f377
andradebru <brunansandrade@gmail.com>
2022-03-18 20:01:06 -0300

    initial commit

---
+++ src/components/NovoComponente.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="novo-componente">
+        <h1>Novo componente</h1>
+        <p>Teste</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NovoComponente'
+}
+</script>
+
+<style scoped>
+
+</style>
+
